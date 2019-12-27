e5430ed33e15ca6ad4592d721d00e011a06be020
difelix <dfelixsilva08@gmail.com>
2019-12-27 15:25:33 -0300

    adicionando init.sql e alterando arquivos de properties

---
+++ database/init.sql
@@ -0,0 +1,36 @@
+CREATE USER admincadastro with encrypted password 'teste';
+
+CREATE DATABASE cadastroclientes OWNER admincadastro;
+GRANT CONNECT ON DATABASE cadastroclientes TO admincadastro;
+
+USE cadastroclientes;
+
+CREATE SCHEMA clientes;
+
+GRANT USAGE ON SCHEMA clientes TO admincadastro;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA clientes TO admincadastro;
+
+create table clientes.cliente (
+    id bigserial PRIMARY KEY,
+	nome varchar NOT NULL,
+	email varchar NOT NULL UNIQUE,
+	cpf varchar(11) NOT NULL UNIQUE
+);
+
+create table clientes.endereco (
+    id bigserial PRIMARY KEY,
+	rua varchar NOT NULL,
+	cep varchar(8) NOT NULL,
+	bairro varchar NOT NULL,
+	cidade varchar NOT NULL,
+	estado varchar NOT NULL,
+	complemento varchar,
+	referencia varchar,
+	id_cliente bigint references clientes.cliente (id)
+);
+
+create table clientes.telefone (
+    id bigserial PRIMARY KEY,
+	numero varchar(11) NOT NULL,
+	id_cliente bigint references clientes.cliente (id)
+);
